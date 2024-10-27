76619e3b088bc174307cf3c7717e2e5a0840c1ed
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-10-27 17:18:35 +0400

    Create AuthToken.cs

---
+++ AuthToken.cs
@@ -0,0 +1,25 @@
+namespace MySuperBlogApp.Models
+{
+    public class AuthToken
+    {
+        public int Minutes { get; private set; }
+
+        public string AccessToken { get; private set; }
+
+        public string UserName { get; private set; }
+
+        public int UserId { get; private set; }
+
+        public AuthToken(
+            int minutes, 
+            string accessToken, 
+            string userName, 
+            int userId)
+        {
+            Minutes = minutes;
+            AccessToken = accessToken;
+            UserName = userName;
+            UserId = userId;
+        }
+    }
+}
