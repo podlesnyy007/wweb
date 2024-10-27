1a83228d4f512c481ad4f585f03439d063fe6fae
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-10-27 17:20:44 +0400

    Create User.cs

---
+++ User.cs
@@ -0,0 +1,17 @@
+namespace MySuperBlogApp.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Description { get; set; }
+
+        public byte[]? Photo { get; set; }
+    }
+}
